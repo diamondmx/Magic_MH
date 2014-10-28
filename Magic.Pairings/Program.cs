7670cb1a86d58321bda8a6cce1490e7ecf11b3c1
diamondmx <diamondmx@gmail.com>
2014-10-28 17:32:38 -0500

    Updated Pairings from office PC

--- Magic.Pairings/Program.cs
+++ Magic.Pairings/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Magic.Pairings
 {
+	[Table(Name = "Players")]
+	public class dbPlayer
+	{
+		[Column(IsPrimaryKey = true)]
+		public string Name;
+	}
+
+	[Table(Name = "Matches")]
+	public class dbMatch
+	{
+		[Column()]
+		public string Player1;
+		[Column()]
+		public string Player2;
+		[Column()]
+		public int Round;
+		[Column()]
+		public string Event;
+		[Column()]
+		public int Player1Wins;
+		[Column()]
+		public int Player2Wins;
+		[Column()]
+		public int Draws;
+		[Column()]
+		public bool InProgress;
+	}
+
+
 	class Player
 	{
 		public string name;
@@ -24,32 +57,123 @@ namespace Magic.Pairings
 			CurrentPlayers = new List<Player>();
 		}
 
+		public Player(string inputDataString, bool secondPlayer)
+		{
+			var inputList = inputDataString.Split(new char[] {','}, StringSplitOptions.None);
+			var p1Name = inputList[0].Trim(new char[] { '\'', '(', ')', ' ' });
+			var p2Name = inputList[1].Trim(new char[]{'\'','(',')', ' '});
+			var round = Convert.ToInt32(inputList[2]);
+			var eventName = inputList[3].Trim(new char[] { '\'', '(', ')', ' ' });
+			var p1score = Convert.ToInt32(inputList[4]);
+			var p2score = Convert.ToInt32(inputList[5]);
+			var draws = inputList[6];
+
+			name = secondPlayer ? p2Name : p1Name;
+			Score = 0;
+			;
+
+			round1Players = new List<Player>();
+			round2Players = new List<Player>();
+			CurrentPlayers = new List<Player>();
+		}
+
 	}
 
 	class Program
 	{
-		static List<Player> Load(string filename)
+		public static List<Player> LoadFile(string fileName)
+		{
+			var list = new List<Player>();
+
+			using (var file = File.Open(fileName, FileMode.Open))
+			{
+				byte[] buffer = new byte[1024*1024];
+				var bytesRead = file.Read(buffer, 0, 1024*1024);
+				var readData = System.Text.Encoding.UTF8.GetString(buffer,0,bytesRead);
+				var readDataLines = readData.Split(new string[]{Environment.NewLine},1024, StringSplitOptions.RemoveEmptyEntries);
+				foreach (string dataLine in readDataLines)
+				{
+					var foundPlayer = new Player(dataLine, false);
+					var foundPlayer2 = new Player(dataLine, true);
+					if(list.Count(p => p.name==foundPlayer.name)<=0)
+						list.Add(foundPlayer);
+
+					if (list.Count(p => p.name == foundPlayer2.name) <= 0)
+						list.Add(foundPlayer2);
+				}
+
+				foreach (string dataLine in readDataLines)
+				{
+					AddMatch(list, dataLine);
+				}
+				
+			}
+			return list;
+		}
+
+		public static List<Player> LoadDatabase()
 		{
 			var outputPlayers = new List<Player>();
-			using (var file = File.Open("magicLoadFile.txt", FileMode.Open))
-			{
-				byte[] fileBytes = null;
-				file.Read(fileBytes, 0, 1024*1024);
-				string fileString = fileBytes.ToString();
-				fileString.Split(new string[]{Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
-			}
+			
+			var db = new DataContext("Magic");
+			var playersTable = db.GetTable<dbPlayer>();
+
+
+			foreach (var p in playersTable)
+				System.Console.Write(p.Name + Environment.NewLine);
+
+			var ignore = System.Console.ReadKey();
+
 			return outputPlayers;
+		}
+
+		static void AddMatch(List<Player> playerList, string matchInput)
+		{
+			var inputList = matchInput.Split(new char[] { ',' }, StringSplitOptions.None);
+			var p1Name = inputList[0].Trim(new char[] { '\'', '(', ')', ' ' });
+			var p2Name = inputList[1].Trim(new char[] { '\'', '(', ')', ' ' });
+			var round = Convert.ToInt32(inputList[2]);
+			var eventName = inputList[3].Trim(new char[] { '\'', '(', ')', ' ' });
+			var p1score = Convert.ToInt32(inputList[4]);
+			var p2score = Convert.ToInt32(inputList[5]);
+			var draws = inputList[6];
+
+			var player1 = playerList.First(player => player.name == p1Name);
+			var player2 = playerList.First(player => player.name == p2Name);
+
+			player1.round1Players.Add(player2);
+			player2.round1Players.Add(player1);
+			if (p1score > p2score)
+				player1.Score++;
+			else if (p2score > p1score)
+				player2.Score++;
 		}
 
 		static void Main(string[] args)
 		{
-			var playerList = GetPlayerList();
+			var playerList = LoadFile("playerInputFile.txt");
+			//var playerList = GetPlayerList();
+
+			var playerListString = "";
+			foreach (Player p in playerList)
+			{
+				playerListString += PlayerListInfoToString(p);
+				playerListString += Environment.NewLine;
+			}
+
+			using (var file = File.Open("magicPlayerList.txt", FileMode.Create))
+			{
+				var outputByte = Encoding.Default.GetBytes(playerListString.ToCharArray());
+				file.Write(outputByte, 0, outputByte.Length);
+			}
+			
+			
 			playerList = GeneratePairings(playerList, 4);
 
 			var outputString = "";
 			foreach (Player p in playerList)
 			{
-				outputString += PlayerInfoToString(p);
+				outputString += PlayerPairingInfoToString(p);
 				outputString += Environment.NewLine;
 			}
 
@@ -63,7 +187,7 @@ namespace Magic.Pairings
 
 		}
 
-		static private string PlayerInfoToString(Player p)
+		static private string PlayerPairingInfoToString(Player p)
 		{
 			String output = "";
 
@@ -75,6 +199,25 @@ namespace Magic.Pairings
 				if(i+1<p.CurrentPlayers.Count)
 					output += ", ";
 			}
+
+			return output;
+		}
+
+		static private string PlayerListInfoToString(Player p)
+		{
+			String output = "";
+
+			output += p.name;
+			output += ": ";
+			for (int i = 0; i < p.round1Players.Count; i++)
+			{
+				output += p.round1Players[i].name;
+				if (i + 1 < p.round1Players.Count)
+					output += ", ";
+			}
+
+			output += String.Format(" ({0})", p.Score);
+
 
 			return output;
 		}
@@ -99,6 +242,8 @@ namespace Magic.Pairings
 			var pete = new Player("Pete", 0);
 			var sean = new Player("Sean", 0);
 			var erik = new Player("Erik", 0);
+
+
 			
 			return new List<Player>{aaron, adam, alex, artem, cbLadd, charlie,david,elise,jeffrey,john,markH,markR,michael,nicholas,nikita,pete,sean,erik};
 		}
