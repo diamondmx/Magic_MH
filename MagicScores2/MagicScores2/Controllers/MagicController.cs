703c3548fbe27eba816a0aa229af14d67593d7e6
diamondmx <diamondmx@gmail.com>
2015-10-29 17:05:36 -0500

    Now reading and saving draws from match result page

--- MagicScores2/MagicScores2/Controllers/MagicController.cs
+++ MagicScores2/MagicScores2/Controllers/MagicController.cs
@@ -7,98 +7,101 @@ using Magic;
 
 namespace MagicScores2.Controllers
 {
-    public class MagicController : Controller
-    {
-        //
-        // GET: /Magic/
-        public ActionResult Index(string eventName, int round)
-        {
-            var thisEvent = new Magic.Core.Event();
-            thisEvent.LoadEvent(eventName);
+	public class MagicController : Controller
+	{
+		//
+		// GET: /Magic/
+		public ActionResult Index(string eventName, int round)
+		{
+			var thisEvent = new Magic.Core.Event();
+			thisEvent.LoadEvent(eventName);
 
-	        ViewBag.Title = String.Format("{0}: Round {1}", eventName, round);
-          ViewBag.Players = thisEvent.Players;
-          ViewBag.EventName = eventName;
-          ViewBag.Round = round;
-	        ViewBag.Event = thisEvent;
-          return View();
-        }
+			ViewBag.Title = String.Format("{0}: Round {1}", eventName, round);
+			ViewBag.Players = thisEvent.Players;
+			ViewBag.EventName = eventName;
+			ViewBag.Round = round;
+			ViewBag.Event = thisEvent;
+			return View();
+		}
 
-        public List<SelectListItem> GetGameWinsDropdownWithSelected(int winsSelected)
-        {
-            var output = new List<SelectListItem>();
-            output.Add(new SelectListItem { Text = "0", Value = "0", Selected = winsSelected == 0 });
-            output.Add(new SelectListItem { Text = "1", Value = "1", Selected = winsSelected == 1 });
-            output.Add(new SelectListItem { Text = "2", Value = "2", Selected = winsSelected == 2 });
+		public List<SelectListItem> GetGameWinsDropdownWithSelected(int winsSelected)
+		{
+			var output = new List<SelectListItem>();
+			output.Add(new SelectListItem { Text = "0", Value = "0", Selected = winsSelected == 0 });
+			output.Add(new SelectListItem { Text = "1", Value = "1", Selected = winsSelected == 1 });
+			output.Add(new SelectListItem { Text = "2", Value = "2", Selected = winsSelected == 2 });
 
-            return output;
-        }
+			return output;
+		}
 
-        //
-        // GET: /Magic/Details/5
-        public ActionResult Details(string eventName, int round, string player1, string player2, int? player1wins, int? player2wins)
-        {
-            var thisEvent = new Magic.Core.Event();
-            thisEvent.LoadEvent(eventName);
+		//
+		// GET: /Magic/Details/5
+		public ActionResult Details(string eventName, int round, string player1, string player2, int? player1wins, int? player2wins, int? draws)
+		{
+			var thisEvent = new Magic.Core.Event();
+			thisEvent.LoadEvent(eventName);
 
-            var match = thisEvent.Matches.Where(m => (m.Round == round) && (m.Player1Name == player1 && m.Player2Name == player2) || (m.Player2Name == player1 && m.Player1Name == player2)).First();
-            ViewBag.Match = match;
-            
-            if (player1wins.HasValue && player2wins.HasValue)
-            {
-							if (thisEvent.Locked(round))
-							{
-								ModelState.AddModelError("CustomError", "This match is Locked");
-							}
-							else
-							{
-								match.Player1Wins = player1wins.Value;
-								match.Player2Wins = player2wins.Value;
+			var match = thisEvent.Matches.Where(m => (m.Round == round) && (m.Player1Name == player1 && m.Player2Name == player2) || (m.Player2Name == player1 && m.Player1Name == player2)).First();
+			ViewBag.Match = match;
 
-								match.Update();
+			if (player1wins.HasValue && player2wins.HasValue && draws.HasValue)
+			{
+				if (thisEvent.Locked(round))
+				{
+					ModelState.AddModelError("CustomError", "This match is Locked");
+				}
+				else
+				{
+					match.Player1Wins = player1wins.Value;
+					match.Player2Wins = player2wins.Value;
+					match.Draws = draws.Value;
 
-								return RedirectToAction("Index", new { controller = "Magic", eventName = eventName, round = round });	
-							}
-            }
+					match.Update();
 
-						var p1dropdown = GetGameWinsDropdownWithSelected(match.Player1Wins);
-            var p2dropdown = GetGameWinsDropdownWithSelected(match.Player2Wins);
+					return RedirectToAction("Index", new { controller = "Magic", eventName = eventName, round = round });
+				}
+			}
 
-            ViewBag.player1wins = p1dropdown;
-            ViewBag.player2wins = p2dropdown;
+			var p1Dropdown = GetGameWinsDropdownWithSelected(match.Player1Wins);
+			var p2Dropdown = GetGameWinsDropdownWithSelected(match.Player2Wins);
+			var drawsDropdown = GetGameWinsDropdownWithSelected(match.Draws);
 
-            return View("MagicMatch");
-        }
+			ViewBag.player1wins = p1Dropdown;
+			ViewBag.player2wins = p2Dropdown;
+			ViewBag.draws = drawsDropdown;
 
-        //
-        // GET: /Magic/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+			return View("MagicMatch");
+		}
 
-        //
-        // GET: /Magic/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		//
+		// GET: /Magic/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-        //
-        // POST: /Magic/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+		//
+		// GET: /Magic/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		//
+		// POST: /Magic/Delete/5
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
