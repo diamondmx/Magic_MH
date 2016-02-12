5db2e456922b6f3aaae6eb425dd3adbf615966d4
diamondmx <diamondmx@gmail.com>
2016-02-12 17:38:22 -0600

    merge master->Try_IoC

--- MagicScores2/Magic.Data/LocalSetup.cs
+++ MagicScores2/Magic.Data/LocalSetup.cs
@@ -10,6 +10,7 @@ namespace Magic.Data.LocalSetup
     {
         public const string connectionStringkCura = @"Data Source=P-DV-DSK-MHIL;Initial Catalog=Magic;User ID=mhMagic;Password=mtgMagic";
         public const string connectionStringSekhmet = @"Data Source=SEKHMET\SQLSEKHMET;Initial Catalog=Magic;User ID=mhMagic;Password=mtgMagic";
-        public const string currentConnectionString = connectionStringSekhmet;
+				public const string connectionStringAzure = @"Server=tcp:kmagic.database.windows.net,1433;Database=kMagic;User ID=kmagic@kmagic;Password=^1lRoBoW55;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string currentConnectionString = connectionStringAzure;
     }
 }
