1016ac3471b90237130fdcf97f678287c0617dbc
mhill <mhill@kcura.com>
2016-03-25 13:46:20 -0500

    Fixed menu/popup javascript bug

--- MagicScores2/MagicScores2/Scripts/site.js
+++ MagicScores2/MagicScores2/Scripts/site.js
@@ -8,21 +8,21 @@ if (typeof jQuery == 'undefined')
 var matchDlg = $("#matchDialog");
 matchDlg.dialog({ autoOpen: false });
 
-setupAllMatchPopups()
+setupAllMatchPopups();
 
 function setupAllMatchPopups() {
 	var allBtns = $(".matchPopupLink");
 
 	allBtns.click(function (event) {
-		var target = $(event.target)
-		var p1 = target.attr("data-player")
-		var p2 = target.attr("data-opponent")
-		var r = target.attr("data-round")
-		var e = target.attr("data-event")
+		var target = $(event.target);
+		var p1 = target.attr("data-player");
+		var p2 = target.attr("data-opponent");
+		var r = target.attr("data-round");
+		var e = target.attr("data-event");
 
 		var url = "/Magic/Match/" + e + "/" + r + "/" + p1 + "/" + p2 + "/";
 
-		matchDlg.load(encodeURI(url))
+		matchDlg.load(encodeURI(url) + " #matchForm");
 		matchDlg.dialog("open");
 
 	})
