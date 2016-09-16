3593df0dd70495d4c40fb03812b8b66ace2f043d
diamondmx <diamondmx@gmail.com>
2016-09-16 08:48:59 -0500

    Minor changes - admin role autocreate, assemblyinfo description

--- kMagicSecure/App_Start/IdentityConfig.cs
+++ kMagicSecure/App_Start/IdentityConfig.cs
@@ -120,8 +120,8 @@ namespace IdentitySample.Models
 		{
 			var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
 			var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
-			const string name = "admin@example.com";
-			const string password = "Admin@123456";
+			//const string name = "admin@example.com";
+			//const string password = "Admin@123456";
 			const string roleName = "Admin";
 
 			//Create Role Admin if it does not exist
@@ -132,20 +132,20 @@ namespace IdentitySample.Models
 				var roleresult = roleManager.Create(role);
 			}
 
-			var user = userManager.FindByName(name);
-			if (user == null)
-			{
-				user = new ApplicationUser { UserName = name, Email = name };
-				var result = userManager.Create(user, password);
-				result = userManager.SetLockoutEnabled(user.Id, false);
-			}
+			//var user = userManager.FindByName(name);
+			//if (user == null)
+			//{
+			//	user = new ApplicationUser { UserName = name, Email = name };
+			//	var result = userManager.Create(user, password);
+			//	result = userManager.SetLockoutEnabled(user.Id, false);
+			//}
 
-			// Add user admin to Role Admin if not already added
-			var rolesForUser = userManager.GetRoles(user.Id);
-			if (!rolesForUser.Contains(role.Name))
-			{
-				var result = userManager.AddToRole(user.Id, role.Name);
-			}
+			//// Add user admin to Role Admin if not already added
+			//var rolesForUser = userManager.GetRoles(user.Id);
+			//if (!rolesForUser.Contains(role.Name))
+			//{
+			//	var result = userManager.AddToRole(user.Id, role.Name);
+			//}
 		}
 	}
 
